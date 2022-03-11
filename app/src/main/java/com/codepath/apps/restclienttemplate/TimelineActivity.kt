035ee9bf0b6e9be0e94db8d6277f22ff71124649
weiiyii <cwy.sophiechen@gmail.com>
2022-03-11 12:54:16 -0500

    initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -0,0 +1,80 @@
+package com.codepath.apps.restclienttemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.codepath.apps.restclienttemplate.models.Tweet
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
+
+class TimelineActivity : AppCompatActivity() {
+
+    lateinit var swipeContainer: SwipeRefreshLayout
+    lateinit var client: TwitterClient
+    lateinit var rvTweets: RecyclerView
+    lateinit var adapter: TweetsAdapter
+
+    public val tweets = ArrayList<Tweet>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline)
+
+        client = TwitterApplication.getRestClient(this)
+
+        swipeContainer = findViewById(R.id.swipeContainer)
+        swipeContainer.setOnRefreshListener {
+            Log.i("TimelineActivity", "Refreshing timeline")
+            populateHomeTimeline()
+        }
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light);
+
+        rvTweets = findViewById(R.id.rvTweets)
+        adapter = TweetsAdapter(tweets)
+
+        rvTweets.layoutManager = LinearLayoutManager(this)
+        rvTweets.adapter = adapter
+
+        populateHomeTimeline()
+    }
+
+    fun populateHomeTimeline() {
+        client.getHomeTimeline(object: JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e("TimelineActivity", "Timeline fetch failed $response")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i("TimelineActivity", "Timeline fetched successfully ${json.jsonArray}")
+
+                val jsonArray = json.jsonArray
+                try {
+                    // Clear out currently fetched tweets
+                    adapter.clear()
+                    val tweetsRetrieved = Tweet.fromJsonArray(jsonArray)
+                    tweets.addAll(tweetsRetrieved)
+                    adapter.notifyDataSetChanged()
+                    // Now we call setRefreshing(false) to signal refresh has finished
+                    swipeContainer.setRefreshing(false)
+                } catch (e: JSONException){
+                    Log.e("TimelineActivity", "JSON Exception: $e")
+                }
+            }
+
+        })
+    }
+}
